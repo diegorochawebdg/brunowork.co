31ab8b380ba26dfacbcdcaf1a808e848de202d2b
Diego Rocha <diego.rocha@neon.com.br>
2019-09-08 17:05:01 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleListComponent,
+    SliderComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
