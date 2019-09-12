c5fdc8a614f77c2d79186011b005599f7691fe3e
Diego Rocha <diego.rocha@neon.com.br>
2019-09-11 21:32:23 -0300

    feat(home): continuing home styling

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: [ './article.component.scss' ],
 })
 export class ArticleComponent implements OnInit {
 
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: [ './slider.component.scss' ],
 })
 export class SliderComponent implements OnInit {
 
