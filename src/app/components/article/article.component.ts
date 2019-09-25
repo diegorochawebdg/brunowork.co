d541b61ae0f0767d0b5938ba80f555efa3f7feb5
Diego Rocha <diego.rocha@neon.com.br>
2019-09-24 21:29:25 -0300

    feat(home): styling home

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { TinySliderInstance, tns } from 'node_modules/tiny-slider/src/tiny-slider';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: [ './article.component.scss' ],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements AfterViewInit {
+
+  /**
+   * A instância do slider
+   */
+  slider: TinySliderInstance;
 
   constructor() { }
 
-  ngOnInit() {
+  /**
+   * Método de construção do slider de cards
+   */
+  createSlider() {
+    const sliderConfig: object = {
+      container: '.article__slider-list',
+      center: true,
+      loop: false,
+      items: 1,
+      controls: true,
+      controlsPosition: 'bottom',
+      controlsText: [ 'Previous Slide', 'Next Slide' ],
+      nav: false,
+    };
+
+    this.slider = tns(sliderConfig);
+  }
+
+  ngAfterViewInit() {
+    this.createSlider();
   }
 
 }
