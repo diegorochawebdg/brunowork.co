24980d92865aa4b84ba6a15a912676e85225e620
Diego Rocha <diego.webdg@gmail.com>
2019-09-30 21:41:54 -0300

    feat(header): links in ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IHeaderLinks {
+  label: string;
+  title: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: [ './header.component.scss' ],
 })
 export class HeaderComponent implements OnInit {
+
+  headerLinks: IHeaderLinks[] = [
+    {
+      label: 'all works',
+      title: 'Read more about my works',
+      url: '/',
+    },
+    {
+      label: 'about',
+      title: 'Read more about myself',
+      url: '/about',
+    },
+    {
+      label: 'articles',
+      title: 'View my articles in Medium.com',
+      url: 'https://medium.com/@brunoalvesfernandes/highlights',
+    },
+    {
+      label: 'hello@brunowork.co',
+      title: 'let\'s get in touch!',
+      url: 'mailto:hello@brunowork.co',
+    }
+  ];
 
   constructor() { }
 
